04fb037d749b36e4e312a83398a158d4c7e2f2e4
John Williams <johnnjit7@gmail.com>
2019-08-26 18:54:54 -0400

    Working pon poste route for adding a book to db

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,7 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 const MONGODB_CONNECTION_STRING = process.env.DB;
+var project = 'books';
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
@@ -22,9 +23,28 @@ module.exports = function (app) {
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
     
-    .post(function (req, res){
+    .post(function aysnc (req, res){
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
+      try {
+        MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
+          if(err) {
+            console.log("Database error: " + err);
+          }
+          console.log("Successfull database connection");
+
+          let postPromise = () => {
+            return new Promise((resolve, reject) => {
+
+            })
+          }
+        });
+        
+        var postResult = await postPromise();
+      }
+      catch(e) {
+
+      }
     })
     
     .delete(function(req, res){
