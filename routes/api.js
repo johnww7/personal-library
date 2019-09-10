05e226aff01d081813737a1d9b81705799028db3
John Williams <johnnjit7@gmail.com>
2019-09-10 18:52:34 -0400

    Working on update by id route

--- routes/api.js
+++ routes/api.js
@@ -1,87 +1,90 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
+var expect = require("chai").expect;
+var MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectId;
 //const MONGODB_CONNECTION_STRING = process.env.DB;
-const MONGODB_CONNECTION_STRING = 'mongodb://john:N1teLockon@ds035787.mlab.com:35787/jwfccmongodb';
-var project = 'books';
+const MONGODB_CONNECTION_STRING =
+  "mongodb://john:N1teLockon@ds035787.mlab.com:35787/jwfccmongodb";
+var project = "books";
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+module.exports = function(app) {
+  app
+    .route("/api/books")
+    .get(function(req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
       try {
         MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
-          if(err) {
+          if (err) {
             console.log("Database error: " + err);
           }
           console.log("successful database connection");
 
           var getPromise = () => {
             return new Promise((resolve, reject) => {
-              db.collection(project).find({},{_id: 1, title: 1, commentCount: 1}).toArray((err, res) => {
-                if(err) {
-                  reject(err);
-                }
-                else {
-                  console.log('Returning books');
-                  console.log('Books in library: ' + JSON.stringify(res));
-                  resolve(res);
-                }
-              });
+              db.collection(project)
+                .find({}, { _id: 1, title: 1, commentCount: 1 })
+                .toArray((err, res) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    console.log("Returning books");
+                    console.log("Books in library: " + JSON.stringify(res));
+                    resolve(res);
+                  }
+                });
             });
           };
 
-          getPromise().then((getResult) => {
+          getPromise().then(getResult => {
             db.close();
             res.json(getResult);
           });
         });
-      }
-      catch (e) {
+      } catch (e) {
         console.log(e);
       }
     })
-    
-    .post( (req, res) => {
+
+    .post((req, res) => {
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
       try {
         MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
-          if(err) {
+          if (err) {
             console.log("Database error: " + err);
           }
           console.log("Successfull database connection");
 
           var postPromise = () => {
             return new Promise((resolve, reject) => {
-              db.collection(project).insertOne({title: title, comments: [], commentCount: 0}, (err, res) => {
-                if(err) {
-                  reject(err);
+              db.collection(project).insertOne(
+                { title: title, comments: [], commentCount: 0 },
+                (err, res) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    console.log("1 book submitted");
+                    console.log(res);
+                    let bookId = res.ops[0]._id;
+                    resolve(bookId);
+                  }
                 }
-                else {
-                  console.log('1 book submitted');
-                  console.log(res);
-                  let bookId = res.ops[0]._id;
-                  resolve(bookId);
-                }
-              });
-            })
+              );
+            });
           };
 
-          let postBookResult = async() => {
+          let postBookResult = async () => {
             let result = await postPromise();
             /*let bookPosted = {
               _id: result,
@@ -89,83 +92,120 @@ module.exports = function (app) {
             };*/
             return result;
           };
-          
-          postBookResult().then(function(postResult) {
-            db.close();
-            /*let bookSubmittedToLibrary = {
+
+          postBookResult()
+            .then(function(postResult) {
+              db.close();
+              /*let bookSubmittedToLibrary = {
               _id: res,
               title
             };*/
-            //res.json({_id: postBookResult});
-            console.log('book: ' + JSON.stringify(postResult));
-            res.json({
-              _id: postResult,
-              title: title
+              //res.json({_id: postBookResult});
+              console.log("book: " + JSON.stringify(postResult));
+              res.json({
+                _id: postResult,
+                title: title
+              });
+              //next();
+            })
+            .catch(e => {
+              console.log(e);
             });
-            //next();
-          }).catch(e => {console.log(e)});
-          
         });
-        
-      }
-      catch(e) {
+      } catch (e) {
         console.log(e);
         next(e);
       }
     })
-    
-    .delete(function(req, res){
+
+    .delete(function(req, res) {
       //if successful response will be 'complete delete successful'
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function(req, res) {
       var bookid = req.params.id;
+      console.log("book id: " + bookid);
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
       try {
         MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
-          if(err) {
+          if (err) {
             console.log("Database error: " + err);
           }
           console.log("successful database connection");
 
           var getPromise = () => {
             return new Promise((resolve, reject) => {
-              db.collection(project).findOne({_id: bookid},{_id: 1, title: 1, comments: 1}, (err, res) => {
-                if(err) {
-                  reject(err);
+              db.collection(project).findOne(
+                { _id: ObjectId(bookid) },
+                { _id: 1, title: 1, comments: 1 },
+                (err, res) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    console.log("Returning selected book");
+                    console.log("Book chosen: " + JSON.stringify(res));
+                    resolve(res);
+                  }
                 }
-                else {
-                  console.log('Returning selected book');
-                  console.log('Book chosen: ' + JSON.stringify(res));
-                  resolve(res);
-                }
-              });
+              );
             });
           };
 
-          getPromise().then((getResult) => {
+          getPromise().then(getResult => {
             db.close();
             res.json(getResult);
           });
         });
-      }
-      catch (e) {
+      } catch (e) {
         console.log(e);
       }
     })
-    
-    .post(function(req, res){
+
+    .post(function(req, res) {
       var bookid = req.params.id;
       var comment = req.body.comment;
+      console.log('book id: ' + bookid + ', comments: ' + comment);
       //json res format same as .get
+      try {
+        MongoClient.connect(MONGODB_CONNECTION_STRING, (err, db) => {
+          if (err) {
+            console.log("Database error: " + err);
+          }
+          console.log("successful database connection");
+
+          var getPromise = () => {
+            return new Promise((resolve, reject) => {
+              db.collection(project).findOne(
+                { _id: ObjectId(bookid) },
+                { _id: 1, title: 1, comments: 1 },
+                (err, res) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    console.log("Returning selected book");
+                    console.log("Book chosen: " + JSON.stringify(res));
+                    resolve(res);
+                  }
+                }
+              );
+            });
+          };
+
+          getPromise().then(getResult => {
+            db.close();
+            res.json(getResult);
+          });
+        });
+      } catch (e) {
+        console.log(e);
+      }
+
     })
-    
-    .delete(function(req, res){
+
+    .delete(function(req, res) {
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
